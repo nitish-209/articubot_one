433755021fbc12bd07692fd9627947326a303eb9
nitish-209 <nitishbishnoiinfo@gmail.com>
2024-02-07 23:31:47 +0530

    LIDAR
    Added lidar plugin and lidar.xacro file to add lidar fuctionality to robot

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -1,24 +1,19 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription,DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-    robot_prefix_arg = DeclareLaunchArgument('robot_prefix', default_value = "")
-    robot_prefix = LaunchDescription('robot_prefix')
-
     package_name='articubot_one' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'robot_prefix': robot_prefix}.items()
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -34,10 +29,8 @@ def generate_launch_description():
                         output='screen')
 
 
-
     # Launch them all!
     return LaunchDescription([
-        robot_prefix_arg,
         rsp,
         gazebo,
         spawn_entity,
